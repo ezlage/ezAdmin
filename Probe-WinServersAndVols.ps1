281646f6d4dd89865b4050380b04573b37f0aa0d
Ezequiel Lage <ezequiel@lageteck.com>
2021-02-10 12:32:26 -0300

    First public pre-release

---
+++ Probe-WinServersAndVols.ps1
@@ -0,0 +1,79 @@
+﻿#https://github.com/ezlage/ezAdmin
+#2020-02-10 14:31 UTC
+Try {
+  $ErrorActionPreference='SilentlyContinue';
+  Write-Host "Probing Windows Servers and their mounted volumes... " -NoNewline;
+  $Servers = Get-ADComputer -Filter '(Enabled -eq $True) -and (OperatingSystem -like "Windows Server*")';
+  [System.Collections.ArrayList] $OKServers=@();
+  [System.Collections.ArrayList] $AlertServers=@();
+  [System.Collections.ArrayList] $FailedServers=@();
+  [System.Collections.ArrayList] $WarningServers=@();
+  ForEach ($Server in $Servers) {
+    Try {          
+      $Result=Invoke-Command -ComputerName $Server.Name -ScriptBlock {
+        Try {          
+          [System.Collections.ArrayList] $Return=@();          
+          $DriveInfo=Get-PSDrive -PSProvider FileSystem | Where-Object {$_.Used -ge 1}
+          $DriveInfo | ForEach-Object {
+            if  (($_.Free/($_.Used+$_.Free)) -gt 0.10) {
+              $null=$Return.Add($_.Name);
+              $null=$Return.Add("OK");
+            } ElseIf (($_.Free/($_.Used+$_.Free)) -gt 0.05) {
+              $null=$Return.Add($_.Name);
+              $null=$Return.Add("WARN");
+            } Else {
+              $null=$Return.Add($_.Name);
+              $null=$Return.Add("ALERT");
+            }
+          }         
+          return $Return;
+        } Catch {
+          return "ERROR";
+        }
+      }
+      If ($Result -eq "ERROR") {
+        $null=$FailedServers.Add($Server.Name);
+      } ElseIf ($Result.Contains("ALERT")) {
+        $null=$AlertServers.Add($Server.Name);
+      } ElseIf ($Result.Contains("WARN")) {
+        $null=$WarningServers.Add($Server.Name);
+      } Else {
+        $null=$OKServers.Add($Server.Name);
+      }    
+    } Catch {
+      $null=$FailedServers.Add($Server.Name);
+    }
+  }
+  Write-Host "OK!"
+  Write-Host;
+  if ($FailedServers.Count -gt 0) {
+    Write-Host "It was not possible to evaluate the volumes of these servers:"
+    ForEach($Srv in $FailedServers) {
+      Write-Host $Srv -ForegroundColor Red;
+    }
+    Write-Host;
+  }
+  if ($AlertServers.Count -gt 0) {
+    Write-Host "Some mounted volumes of these servers are over 95%:"
+    ForEach($Srv in $AlertServers) {
+      Write-Host $Srv -ForegroundColor Yellow;
+    }
+    Write-Host;
+  }
+  if ($WarningServers.Count -gt 0) {
+    Write-Host "Some mounted volumes of these servers are over 90%:"
+    ForEach($Srv in $WarningServers) {
+      Write-Host $Srv -ForegroundColor Yellow;
+    }
+    Write-Host;
+  }
+  if ($OKServers.Count -gt 0) {
+    Write-Host "All mounted volumes of these servers are below 90%:"
+    ForEach($Srv in $OKServers) {
+      Write-Host $Srv -ForegroundColor Green;
+    }
+    Write-Host;
+  }
+} Catch {
+  Write-Host $_.Exception.GetType().FullName, $_.Exception.Message -ForegroundColor Red;
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# ezAdmin
+
+A set of scripts to make life easier for SysAdmins in Windows environments.
+
+### Dependencies
+
+- Windows PowerShell
+- Command Prompt (cmd)
+
+### Usage
+
+Just choose a script and run it! Some will be able to work remotely, others only locally.
+
+### Tips, tricks and suggestions
+
+- You must first enable and configure PSRemoting, WinRM and Firewall
+- If you notice changes or increments that may be beneficial for any use case, give the suggestion or send a pull request
+
+### Change log and roadmap
+
+#### Pending development
+
+New scripts to/that:
+
+- Automatically identify servers, check status and generate DFS reports
+- Make it easier to check the hosts file for multiple servers
+- Facilitate cleaning user profiles on servers
+- Check the last restart of multiple servers
+- Detect, disable, and move inactive computer accounts in AD
+- Detect, disable, and move inactive user accounts in AD
+- Automate cleaning routines for WSUS servers
+- Deploy MSI and EXE packages to servers
+
+#### v0.0.0: First public pre-release
+- Probe-WinServersAndVols.ps1 created, tested and added
+
+### License, credits, feedback and donation
+
+Creative Commons Zero v1.0 Universal  
+Developed by [Ezequiel Lage](https://twitter.com/ezlage), Sponsored by [Lageteck](https://lageteck.com)  
+For feedback, contact@lageteck.com
+
+#### Please, support this initiative!
+BTC: 1KMBgg1h3TGPCWZyi4iFo55QvYrdo5JyRc  
+DASH: Xt7BNFyCBxPdnubx5Yp1MjTn7sJLSnEd5i  
+DCR: Dscy8ziqa2qz1oFNcbTXDyt3V1ZFZttdRcn  
+ETH: 0x06f1382300723600b3fa9c16ae254e20264cb955  
+LTC: LZJPrFv7a7moL6oUHPo8ecCC9FcbY49uRe  
+USDC: 0x38be157daf7448616ba5d4d500543c6dec8214cc  
+ZEC: t1eGBTghmxVbPVf4cco3MrEiQ8MZPQAzBFo   
