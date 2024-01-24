09c8c79401ad73a3fa6877c7c8bd41c489f0605d
Ezequiel Lage <ezequiel@lageteck.com>
2024-01-24 19:32:02 -0300

    Updated unfinished script (PowerShell\Mount-BulkMappings.ps1)

--- PowerShell/Mount-BulkMappings.ps1
+++ PowerShell/Mount-BulkMappings.ps1
@@ -1,17 +1,19 @@
 [CmdletBinding()]
 param (
     [Parameter(Position=0,mandatory=$true)]
-    [string] $VolListFile,
-    [Parameter(Position=0,mandatory=$true)]
-    [string] $EntityMapFile,    
-    [Parameter(mandatory=$false)]
-    [string] $ColSeparator=";",
-    [Parameter(mandatory=$false)]
-    [string] $RowSeparator="`n"
+    [string] $MappingsCSV
 )
 
 Clear-Host;
 $PSDefaultParameterValues['*:Encoding'] = 'utf8';
+
+# Changing the default action on error, warning or progress
+$EPref = $ErrorActionPreference;
+$WPref = $WarningPreference;
+$PPref = $ProgressPreference;
+$ErrorActionPreference = 'Stop';
+$WarningPreference = 'SilentlyContinue';
+$ProgressPreference = 'SilentlyContinue';
 
 Write-Host;
 Write-Host "=========================================";
@@ -23,40 +25,109 @@ Write-Host "= Material protected by a license (MIT) =";
 Write-Host "=========================================";
 Write-Host;
 
-#Entity;DriveLetter;FullPath;Persistent
+$csvHeader = @("Entity", "DriveLetter", "FullPath", "CredPrompt", "Persistent");
+function Test-CSV {
+    param(
+        [string]$FilePath
+    )
+
+    if (-not (Test-Path $FilePath)) {            
+        return $false;
+    }
+
+    try {
+        $csvData = Import-Csv -Path $FilePath -ErrorAction Stop;
+    } catch {        
+        return $false;
+    }
+
+    $DesiredColumns = $csvHeader;
+    $MissingColumns = Compare-Object -ReferenceObject $DesiredColumns -DifferenceObject $csvData[0].PSObject.Properties.Name;
+
+    if ($MissingColumns.Count -gt 0) {                
+        return $false;
+    }
+
+    return $true;
+}
+
+if (Test-CSV -FilePath $MappingsCSV) {
+    try {
+        $Entities = @();
+        $Entities += "$env:userdomain\$env:username";    
+        [System.Security.Principal.WindowsIdentity]::GetCurrent().Groups | Where-Object {$null -ne $_.AccountDomainSid} |        
+            ForEach-Object -Process {$Entities += $_.Translate([System.Security.Principal.NTAccount]).Value}
+        $MappingsList = Import-Csv -Path $MappingsCSV;
+        foreach ($Mapping in $MappingsList) {
+            if (($Mapping.Entity).Split("\").Length -eq 1) {
+                $Mapping.Entity = "$env:userdomain\$($Mapping.Entity)";                
+            }
+            $Mapping.DriveLetter = $Mapping.DriveLetter.ToUpper()[0];            
+            switch ($Mapping.CredPrompt.toLower()) {
+                {@("true","yes","y","1") -contains $_} {
+                    $Mapping.CredPrompt = $true;                     
+                    break;
+                }
+                    default {$Mapping.CredPrompt = $false}
+            }
+            switch ($Mapping.Persistent.toLower()) {
+                {@("false","no","n","0") -contains $_} {
+                    $Mapping.Persistent = $false;                     
+                    break;
+                }
+                    default {$Mapping.Persistent = $true}
+            }
+        }
+        $MappingsList;
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+    }   
+} else {
+    Write-Host "Invalid or missing CSV file!" -ForegroundColor Red;
+}
+
+# Restoring default behavior on error
+$ErrorActionPreference = $EPref;
+$WarningPreference = $WPref;
+$ProgressPreference = $PPref;
+
+Write-Host;
+
 #[System.Security.Principal.WindowsIdentity]::GetCurrent().Groups | ForEach-Object -Process {Write-Host $_.Translate([System.Security.Principal.NTAccount]);}
 
 #try {
-    $Content = Get-Content -Path $VolListFile -Delimiter $RowSeparator -Encoding utf8 -Force;
-    $Vols = @{}
-    foreach ($Line in $Content) {
-        $Item = $Line.Split($ColSeparator);
+    #$Content = ConvertFrom-Csv -Header $csvHeader;
+    
+    #Get-Content -Path $VolListFile -Delimiter $RowSeparator -Encoding utf8 -Force;    
+
+    #$Objs = @{}
+    #foreach ($Line in $Content) {
+        #$Item = $Line.Split($ColSeparator);
         
-        if (($Item.Count -eq 3) -or ($Item.Count -eq 4)) {
-            $Obj = New-Object -TypeName System.Object;
-            $Obj | Add-Member -MemberType NoteProperty -Name "Entity" -Value $Item[0];
-            $Obj | Add-Member -MemberType NoteProperty -Name "DriveLetter" -Value $Item[1].ToUpper();
-            $Obj | Add-Member -MemberType NoteProperty -Name "NetworkPath" -Value $Item[2];
-            switch ($Item[3]) {
-                {@("false","no","n","0") -contains $_} { 
-                    $Obj | Add-Member -MemberType NoteProperty -Name "Persistent" -Value "NO";
-                    break;
-                 }
-                default {$Obj | Add-Member -MemberType NoteProperty -Name "Persistent" -Value "YES";}
-            }
-            $Obj;
-        } else {
-            Write-Host "Line '$Line' ignored due to syntax error!" -ForegroundColor Yellow;
-        }
+        #if ((($Item.Count -eq 4) -or ($Item.Count -eq 5)) -and ($Item[0])) {
+        #    $Obj = New-Object -TypeName System.Object;
+        #    $Obj | Add-Member -MemberType NoteProperty -Name "Entity" -Value "$($env:userdomain)\$($Item[0])";
+        #    $Obj | Add-Member -MemberType NoteProperty -Name "DriveLetter" -Value $Item[1].ToUpper();
+        #    $Obj | Add-Member -MemberType NoteProperty -Name "NetworkPath" -Value $Item[2];
+        #    switch ($Item[3]) {
+        #        {@("false","no","n","0") -contains $_} { 
+        #            $Obj | Add-Member -MemberType NoteProperty -Name "Persistent" -Value "NO";
+        #            break;
+        #         }
+        #        default {$Obj | Add-Member -MemberType NoteProperty -Name "Persistent" -Value "YES";}
+        #    }            
+        #    $Obj | ConvertTo-Csv -NoHeader;            
+        #} else {
+        #    Write-Host "Line '$Line' ignored due to syntax error!" -ForegroundColor Yellow;
+        #}
 
         #
         # 
         #$Vols.Add($Line.Split($ColSeparator));
         #$Item.Count;
         #$Item;
-    }
+    #}
 #}
 #catch {    
 #}
-
-Write-Host;
