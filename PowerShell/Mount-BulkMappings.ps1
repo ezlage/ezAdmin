016602a6f6ad1650b4852f7636bc1c8781f21a3d
Ezequiel Lage <ezequiel@lageteck.com>
2024-01-29 16:49:56 -0300

    Script finished (PowerShell\Mount-BulkMappings.ps1)

--- PowerShell/Mount-BulkMappings.ps1
+++ PowerShell/Mount-BulkMappings.ps1
@@ -53,6 +53,13 @@ function Test-CSV {
     return $true;
 }
 
+function Get-HostName {
+    param (
+        [string] $FilePath
+    )
+    $FilePath | Select-String -Pattern "(?<=\\\\)(.*?)(?=\\)" | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value;
+}
+
 if (Test-CSV -FilePath $MappingsCSV) {
     try {     
         $Username = $null;
@@ -65,18 +72,18 @@ if (Test-CSV -FilePath $MappingsCSV) {
             ForEach-Object -Process {$Entities += $_.Translate([System.Security.Principal.NTAccount]).Value}
         $MappingsList = Import-Csv -Path $MappingsCSV;
         foreach ($Mapping in $MappingsList) {
-            if (($Mapping.Entity).Split("\").Length -eq 1) {
-                $Mapping.Entity = "$env:userdomain\$($Mapping.Entity)";                
+            if (($Mapping.Entity.Trim()).Split("\").Length -eq 1) {
+                $Mapping.Entity = "$env:userdomain\$($Mapping.Entity.Trim())";                
             }
-            $Mapping.DriveLetter = "$($Mapping.DriveLetter.ToUpper()[0]):";            
-            switch ($Mapping.Persistent.toLower()) {
+            $Mapping.DriveLetter = "$($Mapping.DriveLetter.ToUpper().Trim()):";            
+            switch ($Mapping.Persistent.toLower().Trim()) {
                 {@("false","no","n","0") -contains $_} {
                     $Mapping.Persistent = $false;                     
                     break;
                 }
                     default {$Mapping.Persistent = $true}
             }
-            switch ($Mapping.CredPrompt.toLower()) {
+            switch ($Mapping.CredPrompt.toLower().Trim()) {
                 {@("true","yes","y","1") -contains $_} {
                     $CredPrompt = $true;
                     $Mapping.CredPrompt = $true;                     
@@ -84,19 +91,19 @@ if (Test-CSV -FilePath $MappingsCSV) {
                 }
                     default {$Mapping.CredPrompt = $false}
             }
-            switch ($Mapping.SaveCred.toLower()) {
+            switch ($Mapping.SaveCred.toLower().Trim()) {
                 {@("true","yes","y","1") -contains $_} {
                     $CredPrompt = $true;
                     $Mapping.SaveCred = $true;                     
                     break;
                 }
                     default {$Mapping.SaveCred = $false}
-            }                        
+            }                                     
         }        
         foreach ($Mapping in $MappingsList) {            
             if (($Entities -contains $Mapping.Entity) -and ($MappedLetters -notcontains $Mapping.DriveLetter)) {  
                 if (($CredPrompt) -and (-not $SkipCredPrompt)) {
-                    $Username = Read-Host -Prompt "Enter the username to be used in the mappings (DOMAIN\user.name)";
+                    $Username = Read-Host -Prompt "Enter the username to be used in the mappings";
                     $Password = Read-Host -AsSecureString -Prompt "Enter the password to be used in the mappings";
                     Write-Host;                    
                     $CredPrompt = $false;
@@ -106,8 +113,10 @@ if (Test-CSV -FilePath $MappingsCSV) {
                 Remove-SmbMapping -LocalPath $Mapping.DriveLetter -Force -ErrorAction SilentlyContinue;
                 switch(($Mapping.CredPrompt -and $Username -and $Password)) {
                     $true {
-                        if ($Mapping.SaveCred) {
-                            New-SmbMapping -LocalPath $Mapping.DriveLetter -RemotePath $Mapping.FullPath -Persistent $Mapping.Persistent -UserName $Username -Password $Password -SaveCredentials;
+                        if ($Mapping.SaveCred) {                            
+                            Start-Process -Wait -NoNewWindow -FilePath "$env:SystemRoot\System32\cmdkey.exe" -ArgumentList "/delete:`"$(Get-HostName -FilePath $Mapping.FullPath)`"" -RedirectStandardError NUL;
+                            Start-Process -Wait -NoNewWindow -FilePath "$env:SystemRoot\System32\cmdkey.exe" -ArgumentList "/add:`"$(Get-HostName -FilePath $Mapping.FullPath)`" /user:`"$Username`" /pass:`"$Password`"";
+                            New-SmbMapping -LocalPath $Mapping.DriveLetter -RemotePath $Mapping.FullPath -Persistent $Mapping.Persistent;
                         } else {
                             New-SmbMapping -LocalPath $Mapping.DriveLetter -RemotePath $Mapping.FullPath -Persistent $Mapping.Persistent -UserName $Username -Password $Password;
                         }
@@ -116,14 +125,16 @@ if (Test-CSV -FilePath $MappingsCSV) {
                     default {
                         New-SmbMapping -LocalPath $Mapping.DriveLetter -RemotePath $Mapping.FullPath -Persistent $Mapping.Persistent;
                     }
-                }
+                }    
                 $MappedLetters += $Mapping.DriveLetter;
                 Write-Host "-----------------------------------------";                
             }            
         }
     }
     catch {
-        <#Do this if a terminating exception happens#>
+        Write-Host "There was a failure when carrying out the actions!" -ForegroundColor Red;
+        Write-Host $_.Exception.GetType().FullName -ForegroundColor Red;
+        Write-Host $_.Exception.Message -ForegroundColor Red;  
     }   
 } else {
     Write-Host "Invalid or missing CSV file!" -ForegroundColor Red;
